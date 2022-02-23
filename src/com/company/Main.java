5ef2ea3c0165b6af427641cd75b2c9f64cf9de22
Elv310 <Elvisa310@gmail.com>
2022-02-22 19:02:37 -0500

    word guesser

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static String[] Words = {
+            "about", "beginning", "carry",
+            "dance", "early", "false",
+            "glasses", "happy", "issues",
+            "jewels", "knife", "labor",
+            "major", "never", "oceans",
+            "paint", "quick", "radar",
+            "seize", "taste", "under",
+            "value", "wages"
+
+
+
+
+    };
+
+    public static final Random RANDOM = new Random();
+
+
+    public static final int MAX_ERRORS = 8;
+
+    private String wordToFind;
+    private char[] wordFound;
+    private int nbErrors;
+
+    private ArrayList<String> letters = new ArrayList<>()
+
+}
